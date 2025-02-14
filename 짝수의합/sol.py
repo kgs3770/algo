d0ce2035e3d6788a69fb006671d507c5ae308dd0
kkk <kgs3770@gmail.com>
2025-02-14 16:17:09 +0900

    update

--- 짝수의합/sol.py
+++ 짝수의합/sol.py
@@ -1,13 +1,12 @@
 def solution(n):
     answer = 0
-    return answer
 
     for i in range(n+1):
         if i % 2 == 0:
             answer = answer + i
             # answer += i
 
-
+    return answer
 
 print(solution(10))
 print(solution(4))
